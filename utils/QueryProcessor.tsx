9bf4d0dbf81a5e1fc532a0213834cd2d747bd363
Bchenjamin <bzchen@andrew.cmu.edu>
2024-10-10 10:27:43 -0400

    prime and power

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -45,6 +45,38 @@ export default function QueryProcessor(query: string): string {
     return "No Matc";
   }
 
+  if (query.includes("to the power of")) {
+    let match = query.match(/\d+/g); 
+    let numbers = match ? match.map(Number) : [];
+    return Math.pow(numbers[0], numbers[1]).toString();
+  }
+
+  function isPrime(num: number): boolean {
+    if (num <= 1) {
+        return false; // Numbers less than or equal to 1 are not prime
+    }
+    
+    // Check divisibility from 2 to the square root of the number
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false; // If divisible, it's not prime
+        }
+    }
+    return true; // If no divisors were found, it's prime
+}
+
+  if (query.includes("are primes:")) {
+    let match = query.match(/\d+/g); 
+    let numbers = match ? match.map(Number) : [];
+    let ans = [];
+    for (let i = 0; i < numbers.length; i++) {
+      if (isPrime(numbers[i])) {
+        ans.push(numbers[i]);
+      }
+    }
+    return ans.length > 0 ? ans.join(", ") : "No Match";
+  }
+
   if (query.includes("multiplied by")) {
     let match = query.match(/\d+/g);
     let numbers = match ? match.map(Number) : [];
